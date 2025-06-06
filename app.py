0eb0d8603cae041861b45ca792e637b05e998741
sureshijjada <122148309+sureshijjada@users.noreply.github.com>
2025-06-06 14:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,4 +72,6 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)),debug=True)
+
